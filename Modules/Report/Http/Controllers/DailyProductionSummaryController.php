95bd587605a7065335e04e84c2ef9243e48b42bb
Mohammad Arman <arman.ctgbd1394@gmail.com>
2021-10-20 22:26:51 +0600

    Daily Production Summary Report Modified

--- Modules/Report/Http/Controllers/DailyProductionSummaryController.php
+++ Modules/Report/Http/Controllers/DailyProductionSummaryController.php
@@ -12,27 +12,25 @@ class DailyProductionSummaryController extends BaseController
     {
         if(permission('daily-production-summary-access')){
             $this->setPageData('Daily Production Summary','Daily Production Summary','fas fa-file',[['name' => 'Daily Production Summary']]);
-            $total_production_material_cost = DB::table('stock_outs')
-            ->whereBetween('date',[date('Y-m-d'),date('Y-m-d')])
-            ->sum('grand_total');
+            $materials =  DB::table('stock_out_materials as som')
+            ->selectRaw('m.material_name,m.material_code,SUM(som.qty) as qty,AVG(som.net_unit_cost) as cost,so.date')
+            ->join('stock_outs as so','som.stock_out_id','=','so.id')
+            ->join('materials as m','som.material_id','=','m.id')
+            ->groupBy('som.material_id','so.date')
+            ->whereBetween('so.date',[date('Y-m-d'),date('Y-m-d')])
+            ->get();
 
-            $production_product_value = DB::table('adjustment_products as ap')
-            ->selectRaw('p.name,SUM(ap.base_unit_qty) as qty,p.base_unit_price as price,(SUM(ap.base_unit_qty) * p.base_unit_price) as total,a.date')
+            $products = DB::table('adjustment_products as ap')
+            ->selectRaw('p.name,p.code,SUM(ap.base_unit_qty) as qty,p.base_unit_price as price,(SUM(ap.base_unit_qty) * p.base_unit_price) as total,a.date')
             ->join('adjustments as a','ap.adjustment_id','=','a.id')
             ->join('products as p','ap.product_id','=','p.id')
             ->groupBy('ap.product_id','a.date')
             ->whereBetween('a.date',[date('Y-m-d'),date('Y-m-d')])
             ->get();
-            $total_production_product_value = 0;
-            if(!$production_product_value->isEmpty())
-            {
-                foreach ($production_product_value as $value) {
-                    $total_production_product_value += $value->total;
-                }
-            }
+
             $data = [
-                'total_production_material_cost' => number_format($total_production_material_cost,2,'.',''),
-                'total_production_product_value' => number_format($total_production_product_value,2,'.','')
+                'materials' => $materials,
+                'products' => $products
             ];
             return view('report::production-summary-report',$data);
         }else{
--- Modules/Report/Resources/views/production-summary-report.blade.php
+++ Modules/Report/Resources/views/production-summary-report.blade.php
@@ -36,10 +36,87 @@
             <div class="card-body">
                 <!--begin: Datatable-->
                 <div id="kt_datatable_wrapper" class="dataTables_wrapper dt-bootstrap4 no-footer">
-                    <div class="col-md-12 text-center">
-                            <h2 class="text-danger"><b>Total Production Material Cost:</b> {{ $total_production_material_cost }} Tk</h2><br>
-                            <h2 class="text-primary"><b>Total Production Finish Goods Value:</b> {{ $total_production_product_value }} Tk</h2>
-                        
+                    <div class="row">
+                        <div class="col-md-12 text-center">
+                            <h3 class="py-3 bg-warning text-white" style="margin: 10px auto 10px auto;width:300px;">Production Materials</h3>
+                        </div>
+                        <div class="col-sm-12 table-responsive">
+                            <table id="dataTable" class="table table-bordered table-hover">
+                                <thead class="bg-primary">
+                                    <tr>
+                                        <th class="text-center">Sl</th>
+                                        <th>Name</th>
+                                        <th>Code</th>
+                                        <th class="text-right">Cost</th>
+                                        <th class="text-center">Quantity</th>
+                                        <th class="text-right">Total</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @php $total_material_cost = 0; @endphp
+                                    @if (!$materials->isEMpty())
+                                        @foreach ($materials as $key => $value)
+                                            <tr>
+                                                <td class="text-center">{{ $key+1 }}</td>
+                                                <td>{{ $value->material_name }}</td>
+                                                <td>{{ $value->material_code }}</td>
+                                                <td class="text-right">{{ number_format($value->cost,2,'.','') }}</td>
+                                                <td class="text-center">{{ number_format($value->qty,2,'.','') }}</td>
+                                                <td class="text-right">{{ number_format(($value->qty * $value->cost),2,'.','') }}</td>
+                                            </tr>
+                                            @php $total_material_cost += $value->qty * $value->cost; @endphp
+                                        @endforeach
+                                    @endif
+                                </tbody>
+                                <tfoot>
+                                    <tr class="bg-primary">
+                                        <th class="text-right" colspan="5">Total</th>
+                                        <th class="text-right">{{ number_format($total_material_cost,2,'.','') }}</th>
+                                    </tr>
+                                </tfoot>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12 text-center">
+                            <h3 class="py-3 bg-warning text-white" style="margin: 10px auto 10px auto;width:300px;">Production Finish Goods</h3>
+                        </div>
+                        <div class="col-sm-12 table-responsive">
+                            <table id="dataTable" class="table table-bordered table-hover">
+                                <thead class="bg-primary">
+                                    <tr>
+                                        <th class="text-center">Sl</th>
+                                        <th>Name</th>
+                                        <th>Code</th>
+                                        <th class="text-right">Price</th>
+                                        <th class="text-center">Quantity</th>
+                                        <th class="text-right">Total</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @php $total_product_cost = 0; @endphp
+                                    @if (!$products->isEMpty())
+                                        @foreach ($products as $key => $value)
+                                            <tr>
+                                                <td class="text-center">{{ $key+1 }}</td>
+                                                <td>{{ $value->name }}</td>
+                                                <td>{{ $value->code }}</td>
+                                                <td class="text-right">{{ number_format($value->price,2,'.','') }}</td>
+                                                <td class="text-center">{{ number_format($value->qty,2,'.','') }}</td>
+                                                <td class="text-right">{{ number_format($value->total,2,'.','') }}</td>
+                                            </tr>
+                                            @php $total_product_cost += $value->total; @endphp
+                                        @endforeach
+                                    @endif
+                                </tbody>
+                                <tfoot>
+                                    <tr class="bg-primary">
+                                        <th class="text-right" colspan="5">Total</th>
+                                        <th class="text-right">{{ number_format($total_product_cost,2,'.','') }}</th>
+                                    </tr>
+                                </tfoot>
+                            </table>
+                        </div>
                     </div>
                 </div>
                 <!--end: Datatable-->
